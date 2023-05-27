8b54c71f430669a70b6fed2bea87e8857909bd3b
Loanne <58616743+Loanne05@users.noreply.github.com>
2023-05-28 01:23:38 +0800

    Search Box, Infos

--- App.js
+++ App.js
@@ -1,43 +1,121 @@
 import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native';
+import { TextInput } from 'react-native';
+import { StatusBar } from 'react-native';
+
+import * as Speech from "expo-speech";
 
 export default function App() {
-  const [data, setData] = useState([])
-  const [loading, setLoading] = useState(true)
-  const url = "https://api.dictionaryapi.dev/api/v2/entries/en/hello";
+  const [newWord, setNewWord] = useState("") //store the words the user entered in the textInput
+  const [checkedWord, setCheckedWord] = useState("")
+  const [definition, setDefinition] = useState("")
+  const [example, setExample] = useState("")
+  const url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + newWord;
 
-  useEffect(() => {
-    fetch(url)
-      .then((response) => response.json())
-      .then((json) => setData(json))
-      .catch((error) => console.error(error))
-      .finally(() => setLoading(false))
-  }, [])
+  const searchWord = (enteredWord) => {
+    setNewWord(enteredWord)
+  }
+
+  const getInfo = () => {
+    return fetch(url)
+      .then((data) => {
+        return data.json();
+      })
+      .then((response) => {
+        var word = response[0].word
+        setCheckedWord(word);
+
+        var def = response[0].meanings[0].definitions[0].definition;
+        setDefinition(def)
+
+        var eg = response[0].meanings[0].definitions[0].example;
+        setExample(eg)
+      })
+  }
+  const speak=()=>{
+    Speech.speak(checkedWord)
+  }
+
+  const clear=()=>{
+    setCheckedWord("");
+    setDefinition("");
+    setExample("");
+    setNewWord("");;
+  }
 
   return (
-    <View style={styles.container}>
-      {
-        loading ? (<Text> Loading ...</Text>) : (
-          data.map((post) => (
-            <View style={{ flex: 1, alignItems: 'flex-start', justifyContent: 'center' }}>
-              <Text style={{ fontSize: 24, fontWeight: 'bold' }}>{post.word}</Text>
-              {post.meanings.map((meaning, index) => (
-                <View key={index}>
-                  <Text>{meaning.partOfSpeech}</Text>
-                  {meaning.definitions.map((definition, index) => (
-                    <View key={index}>
-                      <Text>{definition.definition}</Text>
-                      <Text style={styles.example}>{definition.example}</Text>
-                    </View>
-                  ))}
-                </View>
-              ))}
+    <ScrollView style={styles.container}>
+      <StatusBar style={'auto'} />
+      <View
+        style={{ alignItems: 'center', }}
+      >
+        <TextInput
+          style={styles.inputbox}
+          placeholder='Search a word'
+          placeholderTextColor='gray'
+          clearButtonMode='always'
+          onChangeText={searchWord}
+          value={newWord}
+        />
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-around',
+            width: '80%',
+            marginVertical: 10,
+            alignItems: 'center'
+          }}
+        >
 
-            </View>
-          ))
-        )
-      }
-    </View>
+          <TouchableOpacity
+            onPress={() => {
+              getInfo()
+            }}
+          >
+            <Text style={styles.goButton}>
+              Go
+            </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={()=>{
+              clear()
+            }}
+          >
+            <Text style={styles.clearButton}>
+              Clear
+            </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity 
+            onPress={()=>{
+              speak()
+            }}
+          >
+            <Image
+              source={require('./assets/audio.png')}
+              style={styles.speakerButton}
+            />
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.dictcontainer} >
+          <Text>
+            Entered Word: {checkedWord}
+          </Text>
+            
+          <Text>
+            Definition: {definition}
+          </Text>
+
+          <Text>
+            Example: {example}
+          </Text>
+        </View>
+      </View>
+
+    </ScrollView>
   );
 }
 
@@ -45,85 +123,39 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  inputbox: {
+    backgroundColor: 'white',
+    width: "80%",
+    height: 50,
+    borderWidth: 3,
+    borderColor: 'gray',
+    marginTop: 100,
+    paddingLeft: 10,
+    paddingRight: 10,
+    borderRadius: 10
+  },
+  speakerButton: {
+    width: 50,
+    height: 50,
+    backgroundColor: 'pink'
+  },
+  goButton: {
+    backgroundColor: 'gray',
+    width: 100,
+    height: 50,
+    textAlign: 'center',
+    verticalAlign: 'middle',
+  },
+  clearButton: {
+    backgroundColor: 'lavender',
+    width: 100,
+    height: 50,
+    textAlign: 'center',
+    verticalAlign: 'middle',
+  },
+  dictcontainer:{
+    marginVertical: 30,
+    paddingHorizontal: 30
+  }
 });
-
-//API sample
-// [{
-//   "word": "hello",
-//   "phonetics": [{
-//     "audio": "https://api.dictionaryapi.dev/media/pronunciations/en/hello-au.mp3",
-//     "sourceUrl": "https://commons.wikimedia.org/w/index.php?curid=75797336",
-//     "license": {
-//       "name": "BY-SA 4.0",
-//       "url": "https://creativecommons.org/licenses/by-sa/4.0"
-//     }
-//   }, {
-//     "text": "/həˈləʊ/",
-//     "audio": "https://api.dictionaryapi.dev/media/pronunciations/en/hello-uk.mp3",
-//     "sourceUrl": "https://commons.wikimedia.org/w/index.php?curid=9021983",
-//     "license": {
-//       "name": "BY 3.0 US",
-//       "url": "https://creativecommons.org/licenses/by/3.0/us"
-//     }
-//   }, {
-//     "text": "/həˈloʊ/",
-//     "audio": ""
-//   }],
-//   "meanings": [{
-//     "partOfSpeech": "noun",
-//     "definitions": [{
-//       "definition": "\"Hello!\" or an equivalent greeting.",
-//       "synonyms": [],
-//       "antonyms": []
-//     }],
-//     "synonyms": ["greeting"],
-//     "antonyms": []
-//   }, {
-//     "partOfSpeech": "verb",
-//     "definitions": [{
-//       "definition": "To greet with \"hello\".",
-//       "synonyms": [],
-//       "antonyms": []
-//     }],
-//     "synonyms": [],
-//     "antonyms": []
-//   }, {
-//     "partOfSpeech": "interjection",
-//     "definitions": [{
-//       "definition": "A greeting (salutation) said when meeting someone or acknowledging someone’s arrival or presence.",
-//       "synonyms": [],
-//       "antonyms": [],
-//       "example": "Hello, everyone."
-//     }, {
-//       "definition": "A greeting used when answering the telephone.",
-//       "synonyms": [],
-//       "antonyms": [],
-//       "example": "Hello? How may I help you?"
-//     }, {
-//       "definition": "A call for response if it is not clear if anyone is present or listening, or if a telephone conversation may have been disconnected.",
-//       "synonyms": [],
-//       "antonyms": [],
-//       "example": "Hello? Is anyone there?"
-//     }, {
-//       "definition": "Used sarcastically to imply that the person addressed or referred to has done something the speaker or writer considers to be foolish.",
-//       "synonyms": [],
-//       "antonyms": [],
-//       "example": "You just tried to start your car with your cell phone. Hello?"
-//     }, {
-//       "definition": "An expression of puzzlement or discovery.",
-//       "synonyms": [],
-//       "antonyms": [],
-//       "example": "Hello! What’s going on here?"
-//     }],
-//     "synonyms": [],
-//     "antonyms": ["bye", "goodbye"]
-//   }],
-//   "license": {
-//     "name": "CC BY-SA 3.0",
-//     "url": "https://creativecommons.org/licenses/by-sa/3.0"
-//   },
-//   "sourceUrls": ["https://en.wiktionary.org/wiki/hello"]
-// }]
